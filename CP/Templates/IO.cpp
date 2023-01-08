d64bced0ede76b0d4544c89b50bd50ce62434ca1
Obaydullah Khan <obaydullahkhaan@gmail.com>
2023-01-08 17:27:31 +0600

    Create IO.cpp

---
+++ CP/Templates/IO.cpp
@@ -0,0 +1,8 @@
+
+void IO(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+        freopen("error.txt", "w", stderr);
+    #endif
+}
