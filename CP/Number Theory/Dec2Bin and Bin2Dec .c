20ef9a22ff3043ecc7240bb8b20aa65bd496a9a1
Obaydullah Khan <obaydullahkhaan@gmail.com>
2023-01-16 22:50:34 +0600

    Update Dec2Bin and Bin2Dec .c

--- CP/Number Theory/Dec2Bin and Bin2Dec .c
+++ CP/Number Theory/Dec2Bin and Bin2Dec .c
@@ -7,6 +7,21 @@ string dec_to_bin(int n) {
     return ret;
 }
 
+
+int bin_to_dec(int n){
+    int num = n;
+    int dec_value = 0;
+    int base = 1;
+    int temp = num;
+    while (temp) {
+        int last_digit = temp % 10;
+        temp = temp / 10;
+        dec_value += last_digit * base;
+         base = base * 2;
+    }
+    return dec_value;
+}
+
 int bin_to_dec(string s) {
     int ret = 0;
     for (int i = 0; i < 32; i++) 
