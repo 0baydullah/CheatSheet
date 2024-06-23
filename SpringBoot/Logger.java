99c71083f538fcb0e88e429d536bafe07ef546f1
Obaydullah Khan <obaydullahkhaan@gmail.com>
2024-06-24 00:18:45 +0600

    Update Logger.java

--- SpringBoot/Logger.java
+++ SpringBoot/Logger.java
@@ -9,8 +9,12 @@ class Logger {
         
         private Logger logger = LoggerFactory.getLogger(Logger.class);
                     // here getLogger method takes class as parameter
+
+        var item = 5;
                     
         logger.info("Logs whatever you want");
+
+        logger.info("Item {}",item); // Item {5}
             
         
     }
